721ea6fe1acba5857f07d04a5f7c4e77b68fa4ff
lile08 <lile08@58.com>
2020-05-11 17:52:55 +0800

    222

--- app/src/main/java/com/keith/comlibrary/MainActivity.java
+++ app/src/main/java/com/keith/comlibrary/MainActivity.java
@@ -1,6 +1,7 @@
 package com.keith.comlibrary;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.e("Keith", getClass().getSimpleName() + " onCreate() : _  222");
     }
 }
